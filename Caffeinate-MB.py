15bb90a3309e19b67ce778973f1c98c406756b59
mehapps <63882957+mehapps@users.noreply.github.com>
2022-06-23 12:54:46 -0400

    Menu bar functionality

--- Caffeinate-MB.py
+++ Caffeinate-MB.py
@@ -11,3 +11,19 @@ def prevent_sleep(length):
 def kill_caffeinate():
     caffeinate.send_signal(signal.SIGINT)
 
+class MenuBarApp(rumps.App):
+    def __init__(self):
+        super(MenuBarApp, self).__init__("Caffeinate")
+        self.menu = ["Caffeinate", "Stop"]
+
+    @rumps.clicked("Caffeinate")
+    def caffeinate(self, _):
+        prevent_sleep(0)
+        print("0")
+
+    @rumps.clicked("Stop")
+    def stop(self, _):
+        kill_caffeinate()
+        print("1")
+
+MenuBarApp().run()
