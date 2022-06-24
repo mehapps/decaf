50dc4e358da1889c74c2fd5d4eacf32371931c73
mehapps <63882957+mehapps@users.noreply.github.com>
2022-06-24 12:32:03 -0400

    Remove rumps class and time function

--- Caffeinate-MB.py
+++ Caffeinate-MB.py
@@ -12,21 +12,13 @@ def kill_caffeinate():
     caffeinate.send_signal(signal.SIGINT)
 
 rumps.debug_mode(True)
-class MenuBarApp(rumps.App):
-    def __init__(self):
-        super(MenuBarApp, self).__init__("Caffeinate")
-        self.menu = ["Forever", "Time", "Stop"]
+@rumps.clicked("Forever")
+def about(sender):
+    prevent_sleep(0)
 
-    @rumps.clicked("Forever")
-    def caffeinate(self, _):
-        prevent_sleep(0)
-    
-    @rumps.clicked("Time")
-    def time_based(self, _):
-        self.title = "Title"
+@rumps.clicked("Stop")
+def about(sender):
+    kill_caffeinate()
 
-    @rumps.clicked("Stop")
-    def stop(self, _):
-        kill_caffeinate()
-
-MenuBarApp().run()
+app = rumps.App("Caffeinater", title='Caffeinate')
+app.run()
